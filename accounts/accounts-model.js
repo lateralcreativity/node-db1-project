a772b6d6444350632a11bb3aa027988531600923
lateralcreativity <lateralcreativity@outlook.com>
2020-08-31 12:26:20 -0500

    CRUD functionality setup

--- accounts/accounts-model.js
+++ accounts/accounts-model.js
@@ -2,7 +2,10 @@ const db = require ('../data/dbConfig');
 
 module.exports = {
     get,
-    getById
+    getById,
+    create,
+    update,
+    remove
 }
 
 function get() {
@@ -10,5 +13,30 @@ function get() {
 }
 
 function getById(id) {
-    return db('accounts').where({ id: id })
+    return db('accounts')
+    .where({ id: id })
+    .first()
+}
+
+function create(account) {
+    return db('accounts')
+    .insert(account, 'id')
+    .then(payload => {
+        return getById(payload)
+    })
+}
+
+function update(changes, id) {
+    return db('accounts')
+    .where({ id: id })
+    .update(changes)
+}
+
+async function remove(id) {
+    const removed = await getById(id);
+
+    return db('accounts')
+    .where({ id: id })
+    .del()
+    .then(() => removed)
 }
--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const id = req.params.id;
-    
+
     db.getById(id)
     .then(payload => {
         if(payload) {
@@ -22,6 +22,41 @@ router.get('/:id', (req, res) => {
     })
     .catch(error => handleError(error, res))
 })
+
+router.post('/', (req, res) => {
+    const account = req.body;
+
+    if(!account.name || !account.budget) {
+        res.status(400).json({ error: 'Name and Budget are required'})
+    } else {
+        db.create(account)
+        .then(payload => {
+            res.status(201).json(payload);
+        })
+        .catch(error => handleError(error, res));
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    db.update(changes, id)
+    .then(payload => {
+        res.status(200).json(payload);
+    })
+    .catch(error => handleError(error, res));
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.remove(id)
+    .then(payload => {
+        res.status(200).json(payload);
+    })
+    .catch(error => handleError(error, res));
+});
 
 function handleError(error, res) {
     return res.status(500).json({ error: error })
